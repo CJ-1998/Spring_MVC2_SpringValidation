616aeafa42f57efc29b80d5a22e10799665035f7
CJ <choijun0627@konkuk.ac.kr>
2023-08-09 17:11:42 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 5
    Bean Validation groups 기능 사용
    
    groups 기능 사용하기 위해 그룹 체크하는 SaveCheck, UpdataCheck
    인터페이스 생성
    Item 객체에 Bean Validation에 groups 속성 추가
    컨트롤러에 @Validated에 인터페이스 값 넣어서 검증할 그룹 알려줌

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,27 +2,25 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
-    @NotNull
+
+    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-//    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) //등록시에만 적용
     private Integer quantity;
 
     public Item() {
