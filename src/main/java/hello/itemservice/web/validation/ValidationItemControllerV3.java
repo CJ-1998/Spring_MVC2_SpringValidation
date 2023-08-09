de5e75c51be70217f6001cb10ed8d98680a5dd6d
CJ <choijun0627@konkuk.ac.kr>
2023-08-09 16:06:05 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 5
    상품 수정에 Bean Validation 적용
    
    상품 수정에도 Bean Validation 적용하기 위해 컨트롤러인 edit 함수에
    @Validated, BindingResult 넣고 ObjectError 검증 로직과 오류 발견 시
    editForm.html로 돌아가도록 함
    editForm.html에 오류 발생 시 메시지 출력되도록 수정

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -47,8 +47,7 @@ public class ValidationItemControllerV3 {
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                bindingResult.reject("totalPriceMin", new Object[]{10000,
-                        resultPrice}, null);
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
@@ -69,9 +68,21 @@ public class ValidationItemControllerV3 {
         model.addAttribute("item", item);
         return "validation/v3/editForm";
     }
-    
+
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
